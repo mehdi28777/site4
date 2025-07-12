b6689e993872b5bb96a2d2a894d007d472ae6c36
mehdi28777 <146303658+mehdi28777@users.noreply.github.com>
2025-07-13 00:25:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="refresh" content="1;url=https://disney.tvdireckt.de/" />
+</head>
+</html>
